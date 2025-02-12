d65909c15423e46a0f4774ca6933a70eee50e136
charles09-love <charleswilbra@gmail.com>
2025-02-12 14:38:00 +0300

    Update object.py

--- lovely/object.py
+++ lovely/object.py
@@ -1,73 +1,73 @@
 from classes import Person, Employee, Car, Rectangle, Emobilis_Employee, Developer, Teacher, Commission_Employee, \
     BankAccount
 
-# person1 = Person()
-# print(person1.first_name)
-# print(person1.last_name)
-# print(person1.gender)
-# print(person1.age)
-#
-# person2 = Person()
-# print(person2.first_name)
-# print(person2.last_name)
-# print(person2.gender)
-# print(person2.age)
-#
-# employee1=Employee("Smith","male",19,30000,"Organizing Director")
-# employee2=Employee("love","male",30,50000,"Developer")
-# employee3=Employee("Jane","female",20,10000,"cleaner")
-# # print(employee1.basic_salary)
-# print(employee2.gender)
-# print(employee3.position)
+person1 = Person()
+print(person1.first_name)
+print(person1.last_name)
+print(person1.gender)
+print(person1.age)
 
-# car1=Car("BMW",2024,"C320",2000)
-# car2=Car("C3 dragon",2022,"A200",39999999)
-# car3=Car("Forester Subaru",2020,"B300",200000000)
-# print(car1.model)
-# print(car2.year)
-# print(car3.typ)
-#
-# print(car1.display())
-# print(car2.display())
-# print(car3.display())
+person2 = Person()
+print(person2.first_name)
+print(person2.last_name)
+print(person2.gender)
+print(person2.age)
 
-#
-# print(employee1.new_salary())
-# print(employee2.new_salary())
-# print(employee3.new_salary())
+employee1=Employee("Smith","male",19,30000,"Organizing Director")
+employee2=Employee("love","male",30,50000,"Developer")
+employee3=Employee("Jane","female",20,10000,"cleaner")
+# print(employee1.basic_salary)
+print(employee2.gender)
+print(employee3.position)
 
-# Rectangle1=Rectangle(50,20)
-# rectangle2=Rectangle(60,30)
-# rectangle3=Rectangle(70,40)
-#
-# print(rectangle3.perimeter())
-# print(rectangle2.area())
-# print(Rectangle1.display())
-#
-# emobilis_employee1 =Emobilis_Employee("McManos","male",3000,23,"Degree")
-# emobilis_employee2 =Emobilis_Employee("ivy","female",68,500000,"Diploma")
-# emobilis_employee3 = Emobilis_Employee("John_stone","male",26,600000,"Masters")
-# print(emobilis_employee1.qualification)
-# print(emobilis_employee2.salary)
-# print(emobilis_employee1.promotion())
-# print(emobilis_employee2.promotion())
-# print(emobilis_employee3.promotion())
-#
-#
-# Developer1 =Developer("king","male",90000,27,"Degree","Frontend Developer","python")
-# Developer2 = Developer("Maureen","female",21,30000,"Diploma","Backend Developer","HTML")
-# print(Developer1.specialization)
-# print(Developer2.prog_language)
-#
-# teacher1 = Teacher("Felix","male",27,30000,"Degree",2023,"English")
-# teacher2 = Teacher("Jake","Male",34,500000,"Masters",2016,"French")
-# print(teacher1.employment_year)
-# print(teacher2.speaking_language)
+car1=Car("BMW",2024,"C320",2000)
+car2=Car("C3 dragon",2022,"A200",39999999)
+car3=Car("Forester Subaru",2020,"B300",200000000)
+print(car1.model)
+print(car2.year)
+print(car3.typ)
 
-# Commission_Employee1=Commission_Employee("king","male",25,50000,"Masters",1000,15)
-# Commission_Employee2=Commission_Employee("charles","male",30,50000,"Masters",1000,12)
-# print(Commission_Employee1.commission_salary())
-# print(Commission_Employee2.commission_salary())
+print(car1.display())
+print(car2.display())
+print(car3.display())
+
+
+print(employee1.new_salary())
+print(employee2.new_salary())
+print(employee3.new_salary())
+
+Rectangle1=Rectangle(50,20)
+rectangle2=Rectangle(60,30)
+rectangle3=Rectangle(70,40)
+
+print(rectangle3.perimeter())
+print(rectangle2.area())
+print(Rectangle1.display())
+
+emobilis_employee1 =Emobilis_Employee("McManos","male",3000,23,"Degree")
+emobilis_employee2 =Emobilis_Employee("ivy","female",68,500000,"Diploma")
+emobilis_employee3 = Emobilis_Employee("John_stone","male",26,600000,"Masters")
+print(emobilis_employee1.qualification)
+print(emobilis_employee2.salary)
+print(emobilis_employee1.promotion())
+print(emobilis_employee2.promotion())
+print(emobilis_employee3.promotion())
+
+
+Developer1 =Developer("king","male",90000,27,"Degree","Frontend Developer","python")
+Developer2 = Developer("Maureen","female",21,30000,"Diploma","Backend Developer","HTML")
+print(Developer1.specialization)
+print(Developer2.prog_language)
+
+teacher1 = Teacher("Felix","male",27,30000,"Degree",2023,"English")
+teacher2 = Teacher("Jake","Male",34,500000,"Masters",2016,"French")
+print(teacher1.employment_year)
+print(teacher2.speaking_language)
+
+Commission_Employee1=Commission_Employee("king","male",25,50000,"Masters",1000,15)
+Commission_Employee2=Commission_Employee("charles","male",30,50000,"Masters",1000,12)
+print(Commission_Employee1.commission_salary())
+print(Commission_Employee2.commission_salary())
 
 
 client1 =BankAccount("Jayson",20000,200,"male",2025,"5 months")
